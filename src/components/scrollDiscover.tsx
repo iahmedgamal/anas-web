784a0063b1e3a49858b4106b6bfe2946ac7a6327
mghlb <79663822+mghlb@users.noreply.github.com>
2023-01-02 14:54:25 +0200

    Refactor scrollDiscover

--- src/components/scrollDiscover.tsx
+++ src/components/scrollDiscover.tsx
@@ -1,21 +1,6 @@
-import AOS from "aos";
-import { useEffect } from "react";
-import "aos/dist/aos.css";
 import "./scrollDiscover.css";
 
 export const ScrollDiscover = () => {
-  useEffect(() => {
-    AOS.init();
-  }, []);
-
-  return (
-    <div
-      className="scroll-discover"
-      data-aos="fade-up"
-      data-aos-duration="600"
-      data-aos-once="true"
-    >
-      Scroll to discover
-    </div>
-  );
+  const text = "SCROLL TO\n\n\n\n\n\nDISCOVER";
+  return <div className="scroll-discover">{text}</div>;
 };
