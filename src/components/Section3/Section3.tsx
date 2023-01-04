68a367331fc5efe1c734622c8edc82b850cdfb5c
Ahmed gamal <ahmed.gamal@musala.com>
2023-01-04 16:57:19 +0200

    update index title & update section 3 with a white image

--- src/components/Section3/Section3.tsx
+++ src/components/Section3/Section3.tsx
@@ -9,11 +9,11 @@ export const Section3 = () => {
 
   return (
     <div
-      data-aos='slide-up'
-      data-aos-anchor-placement='top-center'
-      data-aos-duration='600'
+      data-aos="slide-up"
+      data-aos-anchor-placement="top-center"
+      data-aos-duration="600"
       style={{
-        backgroundImage: `url('https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/WhatsApp%20Image%202022-12-27%20at%2012.55.35.jpeg?alt=media&token=33fb159a-4d87-44bc-9c16-88897f151cb9')`,
+        backgroundImage: `url('https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/Bild053_Neg.Nr.29A-2.jpg?alt=media&token=f85b89c5-2bd1-4c1b-bb88-f90427e560c6')`,
         backgroundRepeat: 'no-repeat',
         backgroundAttachment: 'fixed',
         backgroundSize: 'cover',
