b90a9abe369f93e048b56843e98f806d28d4cd0f
Ahmed gamal <ahmed.gamal@musala.com>
2023-01-24 10:25:43 +0200

    added the first project & fixed the scrolling bug by adding pointer-events

--- src/components/models/Projects.ts
+++ src/components/models/Projects.ts
@@ -2,22 +2,26 @@ import { Project } from './Project';
 
 export const Projects: Project[] = [
   {
-    name: 'Portraits',
+    name: 'Speak & Memory',
     id: 0,
-    time: 'April, 2020',
+    time: 'Frankfurt, Germany, 2019',
     description:
-      'Bordering Sudan from the northern side, the city of Halayeb has\n its own distinctive culture and language; Shot digitally, this \n project primarily aims to capture the soul of the city and \n documents an annual camel trading event secondarily.',
-    url: 'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/ds_6332.jpg?alt=media&token=cb76bf7c-8af5-45b3-bb8d-f65cff834125',
+      "The image depicts a solitary figure, a man\n deep in thought as he sits in the midst of a grand \n hinting at a sense of history and importance. \n The man's posture is one of contemplation and \n introspection as if he is lost in thought about the future.",
+    url: 'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/speak%20and%20memory%2Fthinking.jpg?alt=media&token=9eb2707c-c7c6-4daa-836f-1940c83df565',
     gallery: [
-      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/D85_1193-2%201.png?alt=media&token=e2eece33-28a1-4f79-ace4-290d1fe8449e',
-      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/WhatsApp%20Image%202022-12-27%20at%2012.55.35.jpeg?alt=media&token=33fb159a-4d87-44bc-9c16-88897f151cb9',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/speak%20and%20memory%2Fbe%20mindful.jpg?alt=media&token=d1974e64-c214-4a5d-873f-748abe046a1d',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/speak%20and%20memory%2Fzone.jpg?alt=media&token=543a77af-71fd-4a07-9ca2-77ad11f27bc7',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/speak%20and%20memory%2Fcalm.jpg?alt=media&token=c0453e7c-bd70-479f-a011-7bd2f332aa4f',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/speak%20and%20memory%2Ffar.jpg?alt=media&token=aa2c3b02-36a7-47b4-a138-04d72697cf38',
+
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/speak%20and%20memory%2Flight.jpg?alt=media&token=45c82ba5-2709-4d45-9af9-e0d2cd9b849b',
     ],
   },
   {
     name: 'Halayeb',
     id: 1,
     time: 'April, 2020',
-    description: '',
+    description: 'description of the p',
     url: 'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/WhatsApp%20Image%202022-12-27%20at%2012.55.35.jpeg?alt=media&token=33fb159a-4d87-44bc-9c16-88897f151cb9',
     gallery: [
       'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/D85_1193-2%201.png?alt=media&token=e2eece33-28a1-4f79-ace4-290d1fe8449e',
@@ -27,15 +31,36 @@ export const Projects: Project[] = [
   {
     name: 'Frankfurt',
     id: 2,
-    time: '',
-    description: '',
+    time: 'January, 2020',
+    description: 'description',
     url: 'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/Bild053_Neg.Nr.29A-2.jpg?alt=media&token=f85b89c5-2bd1-4c1b-bb88-f90427e560c6',
     gallery: [
       'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/D85_1193-2%201.png?alt=media&token=e2eece33-28a1-4f79-ace4-290d1fe8449e',
       'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/WhatsApp%20Image%202022-12-27%20at%2012.55.35.jpeg?alt=media&token=33fb159a-4d87-44bc-9c16-88897f151cb9',
     ],
   },
-  { name: 'Singles', id: 3, time: '', description: '', url: '', gallery: [] },
-  { name: 'Covid', id: 4, time: '', description: '', url: '', gallery: [] },
-  { name: 'Yemen', id: 5, time: '', description: '', url: '', gallery: [] },
+  {
+    name: 'Singles',
+    id: 3,
+    time: 'January, 2020',
+    description: '',
+    url: '',
+    gallery: [],
+  },
+  {
+    name: 'Covid',
+    id: 4,
+    time: 'January, 2022',
+    description: '',
+    url: '',
+    gallery: [],
+  },
+  {
+    name: 'Yemen',
+    id: 5,
+    time: 'January, 2021',
+    description: ' description description',
+    url: '',
+    gallery: [],
+  },
 ];
