db3cc6309574435f032c26783fb5ea229878e0b2
Ahmed gamal <ahmed.gamal@musala.com>
2023-02-06 22:57:47 +0200

    images added

--- src/components/models/Projects.ts
+++ src/components/models/Projects.ts
@@ -2,8 +2,50 @@ import { Project } from './Project';
 
 export const Projects: Project[] = [
   {
-    name: 'Speak & Memory',
+    name: 'Behind the stage',
     id: 0,
+    time: 'May, 2021',
+    description:
+      'A stunning visual journey that captures the essence of humanity in all its forms. From the grandeur of the theater, to the confident grace of a young woman, to the intimate moment of a couple holding their newborn, these five images are a testament to the beauty and wonder of life.',
+    url: 'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/behind%20the%20stage%2Ftheater_tiny.jpg?alt=media&token=0e579e20-99eb-4451-bd0d-29cf42b1f378',
+    gallery: [
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/behind%20the%20stage%2Fconfidence_tiny.jpg?alt=media&token=658ff7a2-1138-4a19-b74d-0b31030a37a0',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/behind%20the%20stage%2Fcreation_tiny.jpg?alt=media&token=42d8d793-4f72-4824-b9cc-b6919ebe6009',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/behind%20the%20stage%2Fmirror_tiny.jpg?alt=media&token=a4a0750f-2214-4fd2-826b-b6da99c02769',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/behind%20the%20stage%2Fpeace_tiny.jpg?alt=media&token=b63553af-be28-41fb-9f8f-5d62c34cdbc0',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/behind%20the%20stage%2Ftheater_tiny.jpg?alt=media&token=0e579e20-99eb-4451-bd0d-29cf42b1f378',
+    ],
+  },
+  {
+    name: 'Al Janub',
+    id: 1,
+    time: 'January, 2020',
+    description:
+      'This decryption speaks to the idea that a photograph captures a specific moment in time and space, creating a unique monument to that moment. The particularities of the scene, such as the birds, sea, and fishermen, create a specific image that is different from others interpretations, but the photograph provides tangible proof that this specific moment in time and space existed. The photograph serves as a witness to the light that was present in that moment, serving as a reminder that the prese',
+    url: 'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/Al%20Janub%2F3.jpg?alt=media&token=c455801a-750f-457f-acd5-40a603104334',
+    gallery: [
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/Al%20Janub%2F1.jpg?alt=media&token=8b04efed-d1bd-4f2f-85c7-8a4923c0c497',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/Al%20Janub%2F2.jpg?alt=media&token=1ea9b408-ffa5-4ac0-bf7d-bb1ddc9b900f',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/Al%20Janub%2F3.jpg?alt=media&token=c455801a-750f-457f-acd5-40a603104334',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/Al%20Janub%2F4.jpg?alt=media&token=c3bca658-02fa-4b5a-aee7-46cf33e1121b',
+    ],
+  },
+  {
+    name: 'Forgotten Triangle',
+    id: 2,
+    time: 'January, 2022',
+    description: '',
+    url: 'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/forgotten%20triangle%2F3.jpg?alt=media&token=a15a252e-a5eb-4600-af90-32eef915a9e5',
+    gallery: [
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/forgotten%20triangle%2F1%20(1).jpg?alt=media&token=ed9f4856-a4c8-4c81-bf60-6289af2201af',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/forgotten%20triangle%2F12.jpg?alt=media&token=e9aad24c-0086-42d9-beaa-27f2b562de0c',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/forgotten%20triangle%2F13.jpg?alt=media&token=def996b3-ce29-42ad-a05b-26111c2017f1',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/forgotten%20triangle%2F6.1.jpg?alt=media&token=caee5df2-e44c-4f8c-8cfa-29659e1b1c29',
+    ],
+  },
+  {
+    name: 'Speak & Memory',
+    id: 3,
     time: 'Frankfurt, Germany, 2019',
     description:
       "The image depicts a solitary figure, a man\n deep in thought as he sits in the midst of a grand \n hinting at a sense of history and importance. \n The man's posture is one of contemplation and \n introspection as if he is lost in thought about the future.",
@@ -17,54 +59,33 @@ export const Projects: Project[] = [
       'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/speak%20and%20memory%2Flight.jpg?alt=media&token=45c82ba5-2709-4d45-9af9-e0d2cd9b849b',
     ],
   },
+
   {
-    name: 'Behind the stage',
-    id: 1,
-    time: 'May, 2021',
-    description:
-      'A stunning visual journey that captures the essence of humanity in all its forms. From the grandeur of the theater, to the confident grace of a young woman, to the intimate moment of a couple holding their newborn, these five images are a testament to the beauty and wonder of life.',
-    url: 'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/behind%20the%20stage%2Fcolors_tiny.jpg?alt=media&token=3a504434-9511-48af-bbad-efca50712465',
-    gallery: [
-      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/behind%20the%20stage%2Fconfidence_tiny.jpg?alt=media&token=658ff7a2-1138-4a19-b74d-0b31030a37a0',
-      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/behind%20the%20stage%2Fcreation_tiny.jpg?alt=media&token=42d8d793-4f72-4824-b9cc-b6919ebe6009',
-      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/behind%20the%20stage%2Fmirror_tiny.jpg?alt=media&token=a4a0750f-2214-4fd2-826b-b6da99c02769',
-      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/behind%20the%20stage%2Fpeace_tiny.jpg?alt=media&token=b63553af-be28-41fb-9f8f-5d62c34cdbc0',
-      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/behind%20the%20stage%2Ftheater_tiny.jpg?alt=media&token=0e579e20-99eb-4451-bd0d-29cf42b1f378',
-    ],
-  },
-  {
-    name: 'Frankfurt',
-    id: 2,
-    time: 'January, 2020',
-    description: 'description',
-    url: 'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/Bild053_Neg.Nr.29A-2.jpg?alt=media&token=f85b89c5-2bd1-4c1b-bb88-f90427e560c6',
-    gallery: [
-      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/D85_1193-2%201.png?alt=media&token=e2eece33-28a1-4f79-ace4-290d1fe8449e',
-      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/WhatsApp%20Image%202022-12-27%20at%2012.55.35.jpeg?alt=media&token=33fb159a-4d87-44bc-9c16-88897f151cb9',
-    ],
-  },
-  {
-    name: 'Singles',
-    id: 3,
-    time: 'January, 2020',
-    description: '',
-    url: '',
-    gallery: [],
-  },
-  {
-    name: 'Covid',
+    name: 'Old Skin',
     id: 4,
-    time: 'January, 2022',
-    description: '',
-    url: '',
-    gallery: [],
+    time: 'January, 2016',
+    description:
+      'Amidst the bustling chaos of life, there lies a world of untold stories waiting to be discovered. This album takes you on a journey through the lens of photographer Brahim, a self-taught artist with a passion for capturing the mysterious relationships between people and their environments',
+    url: 'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/old%20skin%2F3.jpeg?alt=media&token=fbb6c9f9-d2a2-4e9d-894a-f77e6fec9869',
+    gallery: [
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/old%20skin%2F1.jpeg?alt=media&token=2262d5f8-5c92-49dc-876c-a0e04c17ac5f',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/old%20skin%2F2.jpeg?alt=media&token=d974bc09-332f-4217-8d30-798e19a533b7',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/old%20skin%2F4.jpeg?alt=media&token=0ab6387f-c557-4fc1-8685-c736add6a0f7',
+    ],
   },
+
   {
-    name: 'Yemen',
+    name: 'They Move On Tracks Of Never-Ending Light',
     id: 5,
     time: 'January, 2021',
     description: ' description description',
-    url: '',
-    gallery: [],
+    url: 'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/They%20Move%20On%20Tracks%20Of%20Never-Ending%20Light%2F1.jpg?alt=media&token=693c2705-d3c3-4921-a8c8-005fdb03e073',
+    gallery: [
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/They%20Move%20On%20Tracks%20Of%20Never-Ending%20Light%2F3(3).jpg?alt=media&token=dfa2675f-7d63-4ed0-8668-381907648134',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/They%20Move%20On%20Tracks%20Of%20Never-Ending%20Light%2F2%20(1).jpg?alt=media&token=41e95e71-0be6-40f0-a97f-a161b5d3c7ee',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/They%20Move%20On%20Tracks%20Of%20Never-Ending%20Light%2F3(5).jpg?alt=media&token=bf4f5540-3536-46dc-a320-76ed9bffe9c6',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/They%20Move%20On%20Tracks%20Of%20Never-Ending%20Light%2F2(2).jpg?alt=media&token=203ea8fd-0d90-40b4-a08d-72e0a3204605',
+      'https://firebasestorage.googleapis.com/v0/b/anas-558b9.appspot.com/o/They%20Move%20On%20Tracks%20Of%20Never-Ending%20Light%2F3(2).jpg?alt=media&token=16879fc5-d818-40a7-9b50-70b0e8e2f967',
+    ],
   },
 ];
